f52181f30d30608b62038c72156c6245070b1903
Matias Scaramuzzino <mscaramuzzino@IEA.local>
2026-03-23 17:35:48 -0300

    chore: agregar .gitignore y excluir .pio/build libdeps node_modules

--- CoffeeControl_v3/.pio/build/esp32c3/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32c3/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/mscaramuzzino/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/mscaramuzzino/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/mscaramuzzino/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/qio_qspi/include/sdkconfig.h
--- CoffeeControl_v3/.pio/build/esp32c3/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32c3/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/mscaramuzzino/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/mscaramuzzino/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
