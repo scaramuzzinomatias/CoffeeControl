09e1a3b32263270c4843cb66898819adcc2ec36d
Matias Scaramuzzino <mscaramuzzino@IEA.local>
2026-07-11 15:24:51 -0300

    chore: remove temp diagnostic script

--- backend/check_rls.js
+++
@@ -1,4 +0,0 @@
-const { createPool } = require('./scripts/_lib');
-const p = createPool();
-p.query("SELECT relname, relrowsecurity FROM pg_class c JOIN pg_namespace n ON n.oid=c.relnamespace WHERE n.nspname='public' AND relrowsecurity = true")
-  .then(r => { console.log(r.rows); p.end(); });
